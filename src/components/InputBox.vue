0dec879c75384d7c5cdd18994a3cfec997d4b8e6
Daniel Alegria Sallo <ccusdanielalegriasallo@gmail.com>
2023-10-26 17:28:24 -0500

    first commit

---
+++ src/components/InputBox.vue
@@ -0,0 +1,37 @@
+<script setup>
+    // import { ref } from 'vue'
+    defineProps({
+        label: {
+            type: String,
+            required: true,
+        },
+        type: {
+            type: String,
+            required: false,
+        },
+        value: {
+            type: String,
+            required: false,
+        },
+        name: {
+            type: String,
+            required: false,
+        },
+    })
+    // defineProps([
+    //     "label", "type"
+    // ])
+</script>
+
+<template>
+    <div class="idk">
+        <label>
+            {{ label }}
+            <input :type="type" :value="value" :name="name"/>
+        </label>
+    </div>
+</template>
+
+<style scoped>
+</style>
+
---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<script setup>
+    import InputBox from "@/components/InputBox.vue"
+
+    function submitForm () {
+        console.log("hola")
+    }
+
+</script>
+
+<template>
+  <main>
+      <form @submit.prevent="submitForm">
+          <InputBox label="Capital" type="number"/>
+          <InputBox label="Tasa De Interes" type="number"/>
+          <InputBox label="Periodo" type="number"/>
+
+          <!-- dias años meses -->
+          <!-- <RadioBox /> -->
+          <InputBox label="Dias" type="radio" value="days" name="periodo"/>
+          <InputBox label="Meses" type="radio" value="months" name="periodo"/>
+          <InputBox label="Años" type="radio" value="years" name="periodo"/>
+
+          <input type="submit" value="Calcular">
+      </form>
+  </main>
+</template>
+
